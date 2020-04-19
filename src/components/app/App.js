1efce0c0f7c9236bb7aa76a43938d8ad5be0361e
logar16 <36807589+logar16@users.noreply.github.com>
2020-04-19 00:15:59 -0700

    Refactor files and directories

---
+++ src/components/app/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div>
+      <h1>
+        Testing... 
+      </h1>
+      <p>
+        Other text...
+      </p>
+    </div>
+  );
+}
