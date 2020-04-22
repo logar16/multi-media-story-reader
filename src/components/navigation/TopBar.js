cca06c3bf59afa41750cb114532370e9ca4e9ce7
logar16 <36807589+logar16@users.noreply.github.com>
2020-04-21 22:12:32 -0700

    Add Material UI

---
+++ src/components/navigation/TopBar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Grid, AppBar, makeStyles, Toolbar, Typography } from '@material-ui/core';
+import * as ROUTES from '../../constants/routes';
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    borderBottom: `2px solid ${theme.palette.divider}`,
+  },
+}));
+
+export function TopBar(props) {
+  var classes = useStyles();
+  
+
+  return (
+    <AppBar 
+      position="static" 
+      color="default" 
+      elevation={0} 
+      className={classes.appBar}
+    >    
+      <Toolbar>
+          <Typography variant='h4'>
+            {props.display}
+          </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+
+{/* <li>
+  <Link to={ROUTES.LANDING}>Landing</Link>
+</li>
+<li>
+  <Link to={ROUTES.HOME}>Home</Link>
+</li>
+<li>
+  <Link to={ROUTES.ADMIN}>Admin</Link>
+</li> */}
+  
