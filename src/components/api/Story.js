f290c9f689fdf18db4b93c6158020da194c3d377
logar16 <36807589+logar16@users.noreply.github.com>
2020-04-21 23:55:07 -0700

    Add stories

--- src/components/api/Story.js
+++ src/components/api/Story.js
@@ -1,18 +1,73 @@
-const items = [
-    {
-      id: 0,
-      title: 'The Maskin Project',
-      subheader: '',
-      description: `Robots with swords.  There really isn't much else to say about it.`,
-    },
-    {
-      id: 1,
-      title: `The Angel's Game`,
-      subheader: '',
-      description: `What would it be like to live a hundred lifetimes, trying to win an almost impossible game?`,
-    },
-  ]
 
 export function getStorySummaries(setStories) {
-    setStories(items);
+    setStories(summaries);
+}
+
+export function getStory(storyId, setStory) {
+    setStory(stories[storyId]);
+}
+
+
+
+const summaries = [
+    {
+        id: 'maskin',
+        title: 'The Maskin Project',
+        subheader: '',
+        description: `Robots with swords.  There really isn't much else to say about it.`,
+    },
+    {
+        id: 'angels',
+        title: `The Angel's Game`,
+        subheader: '',
+        description: `What would it be like to live a hundred lifetimes, trying to win an almost impossible game?`,
+    },
+    ]
+
+const stories = {
+    maskin: {
+        text: `“Torro, prepare to exit the stairwell on floor twelve,” Katch informed him as he sent Freija’s position using his drone and her personal GPS for triangulation. He also sent an approximation of the blueprint of the building to Torro.  As Torro leapt up the last five steps to floor twelve, he was able to see just how much havoc a dreadnaut could create.
+        The whole floor was in shambles compared to the other floors he had seen on the way up.  Splinters from the wall frames were strewn about with a fair amount of other debris.  Old computers lay smashed upon the ground and intermingled with rotting papers and furniture.  Torro ran forward and could hear the dreadnaut crashing through another wall nearby.  “Give me a look at the blueprint with their approximate location,” he directed his AI, Keven.
+        “At once,” it promptly replied.  A translucent blue layout with sketched floors and walls superimposed over the scene before him.  It was generated using Katch’s equipment outside the building looking in, and the data retrieved from Freija as she traversed the floor.  The blueprint adjusted itself to follow his vision as he moved down the hallway.  Two red, robot-shaped outlines appeared, running along the south end of the building.  He confirmed the approximation with Freija.  He watched live footage from her cameras and took note of the dreadnaut’s strategy.  
+        “I don’t suppose we would be very successful taking that monster on directly in battle, especially considering the inhibition of movement inflicted on us by these walls,” Torro thought to Keven and Freija.  “I don’t even know if my sword will be all that useful against its armor,”
+        “I haven’t tried yet.  Let’s see.”
+        He watched Freija test his theory by hiding around a corner and then attacking the dreadnaut as it rounded it.  Its sheets of flexible armor handled the damage well, only denting slightly even though Freija had thousands of pounds of force behind each swing.  The dreadnaught deflected her next attack with its forearm and then punched her maskin in the chest, sending her spinning down the hall.  She again rolled out of her fall, turned, and began running away again. 
+        “I’m not good enough at using a sword to take that thing on,” she admitted to Torro.  “Jord,” her AI, “analyzed its armor and suggests there a few weak points at the edges of plates of armor like our own robots. Maybe that helps.  Still, it hasn’t even bothered pulling its sword out.  Probably because it would be too big to even get a good swing in up here.”  She jumped over a cubicle and darted right. “We would have the same problem.  And it is probably strong enough to rip us apart like toys if it gets us trapped in close quarters.  This isn’t working as easily as I anticipated.”
+        “If I bring it around to you, can you find a way to take it out?” 
+        Freija and the dreadnaut reached the edge of the building.  She was able to quickly turn the corner and sprint down the open hallway that was lit by the bright afternoon light.  The dreadnaut turned the corner, and it was obvious it hadn’t expected the windows to be so close.  It put out its left arm to push off the windows.  The window broke under the pressure and tumbled to the ground, but it had been a sufficient support to allow the dreadnaut to carry on in its new direction.  Torro had an idea come upon him as he watched this occur in Freija’s rear-vision.  He ran to about the middle of the east side of the building.  The blueprint was mostly correct, conveniently.  He had before him a straight hallway that led to the west edge of the building.  The only thing between him and the windows bordering the outside of the building was a small wall that was lined with dilapidated cubicles.
+        “Freija, keep running, but consider this: as you and the enemy cross the midline of the building on the west side I will blow through this wall and knock him out of the building.  Agreed?”  Freija agreed and prepared to round the corner to run along the west edge.  Torro consulted with his AI, “Tell me when to start jogging and then running and then boosting such that I will go through that wall at the same moment as that dreadnaut.”  Keven agreed and made the calculations.  Freija and the dreadnaut were almost to the corner of the building.
+        “Now!”
+        Torro began to jog at a quick pace to bring his speed up.  Freija’s view was a brilliantly lit hallway that seemed to strobe as she sprinted through the alternating shadows cast by the metal holding the windows in place.  Torro had suggested in his plan that she almost let the dreadnaut catch her so its focus would be on her.  That was a dangerous game to play.  “Begin sprinting, now!”
+        Torro detached his sword as his pace continued to increase. He was instantaneously in a full sprint for a wall.  He mentally hesitated as the wall loomed closer.  It was not every day he tried running into things at thirty miles an hour.  It’s really old and flimsy, remember that, he consoled himself.  He was almost there. “Apply adrenaline,” he commanded.  That would give him better reaction time once things started happening at high speeds.
+        “Engage your rockets, now!” 
+        Torro pointed his sword straight ahead and felt his feet leave the ground as he powerfully pumped his legs and initiated his rockets.  First came the wall.  He could feel the tip of his sword as it met the initial resistance.  His body then met the wall, which exploded before his metal frame.  It was much weaker than he had anticipated; but then he hit the dreadnaut.  It was more solid than he expected. 
+        His sword sunk deep into the right lower back of the dreadnaut.  This caused an immediate loss of flexibility for the enemy robot.  Torro’s body then collided with the dreadnaut like two planets meeting in space.  There was a quick transfer of momentum and the two bodies began heading toward the west windows.  It was all happening so fast that Torro could not react until they had already crashed into and then out of the windows.  His sword was so thoroughly jammed into the other robot that he did not even try to remove it.  He jumped off the dreadnaut like a diver pushing off the seafloor to reach the surface. Unluckily, he did not go as far up as he had hoped.  He engaged his rockets, but he had used most of their potential energy to pick up speed to get through the wall.  This did not look promising.  A twelve-story fall was definitely more than a maskin could survive and maintain functionality.
+        Freija had stopped and turned around as soon as she had passed the point where Torro blew through the wall.  She watched the two robots roll out the window in an explosion of glass shards that caught the evening sun dazzlingly.  She saw Torro jumping up in an attempt to make it back in the window.  I wonder if he’ll. . . .  Her AI did the math for her even as Keven did the math for Torro. 
+        “You aren’t going to make it! You are short by 4.5 centimeters,” Keven informed Torro.
+        “A few cm?!  Are you kidding me?!” Torro objected loudly, as he hit into the window and wall barely below the opening he had created.  His maskin had reached the zenith of its parabola, and now gravity was starting to win and pull him mercilessly down.
+        “I’ve got you, love!”
+        Freija jumped down and reached out the window to grab him.  Her reach was successful; however, she was beginning to slide out the window herself as the rusted metal frame gave way under the weight of two massive robots.  It had already been bent wildly out of shape during the expulsion of the dreadnaut, and now snapped.  Freija communicated a mental image to Torro with the suggestion that he knock open the window of the floor below.
+        Torro kicked and cracked the window, but at the sacrifice of Freija sliding out faster.  He kicked it again and it shattered as Freija’s robot lost traction and was pulled from the twelfth floor.  Torro was able to get a handful of glass with one hand and good grip on the metal frame with the other hand.  Freija was able to give herself a little boost backwards and upwards so that she flew nicely in the fresh hole.  No sooner had she contacted the floor then she was up and pulling Torro in to join her.  They sat there for several seconds before speaking.
+        “I must admit, Torro,” Freija said turning to him, “that was actually pretty freakin’ sweet.” He chuckled and replied,
+        “Yeah it was!  I’m just glad I didn’t follow that guy.”  They both looked over the edge at the crumpled mass of the dreadnaut.  It appeared to be able to move its head but that was it.  Katch suddenly chimed in,
+        “That was sick, Torro! Consider our mission here complete!”  Freija nimbly jumped to her feet and started walking down the hallway, this time enjoying the golden rays of eventide.
+        “Shall we descend to retrieve your sword?”  Torro was in a moment by her side. 
+        “I think we shall.”  He took her arm in his with only the slightest scratching of metal.
+        They quickly exited the building into the fair evening light.
+        ` 
+    },
+    angels: {
+        text: `Rylynn caught sight of the tree which marked the point at which she should leave the path.  The sapling she had planted so many lives ago had grown to a massive oak tree in the intervening years.  She reached the tree, stroked its worn bark and patted it lightly.
+        "You look like I feel," she muttered to the tree before pushing off at a ninety-degree angle to the right.  She stepped over roots and rocks as she distractedly counted steps.  Everything looked different, and still quite similar since the last time she had visited her library.  She had barely been able to walk on her own last time at the ripe old age of 82. That had been a long laborious life that ended like all the others. Yet she kept playing.
+        Eventually, she found the remains of her library.  The stone had worn further, and started to cave-in, in places. Vines and ivy wrapped possessively  around the building.  Rylynn brushed aside what was left of the door and it fell from its rusted hinges.  She'd have to fix that next time.  She had brought a lamp; however, the ceiling was starting to cave, providing sufficiently bright beams of sunlight.  She left her bag of supplies at the door, save a fresh book, and made her way down the hall.
+        After Rylynn gathered the keys from their hiding places, she unlocked the panel covering the floor of the closet.  She pulled back the panel to reveal a chest.  She was young and strong enough this visit to pull it out from the hole and open it on the ground floor.  The humidity had not been especially kind to the chest, but it was holding up better than the door.
+        She looked fondly on the books within the chest: her private library.  It contained nearly 800 years of journals.  An accounting of all her most important moments.  Her memory of those days had mostly faded. It brought her consolation to relive those moments.  Victories hard won, friendships created, lovers wooed--it was all here.  That and information on how to secure wealth to get her started on a good foot.  But before that, she would refill her mind with the primary accounts of the legends she had created and inspired.
+        
+    . . . .
+    
+    The day was far spent, so Rylynn created a little camp and settled in for the night.  She had cruised through about a third of the journals.  If she weren't skipping over the boring details she would have had to come prepared for a week of camping out in the decrepit library!  Nevertheless, it had been interesting to notice the change in tone from those first few journals to the latter ones.  It quickly devolved from cocky optimism to a more hardened, realistic understanding of the difficulty of winning the game and living as a human.
+    Some lives were good lives, others tragic and even traumatic.  She laughed as she read and remembered anew clever moves she had made.  She cried as she remembered the loss of loved ones, knowing she'd continue to live on and on without ever seeing them again.  Perhaps if she could become the Overseer, she would have authority to visit the realm of the spirits that passed on.  Perhaps she could see Galen again…. Perhaps.
+    [Insert more memories here…]
+    `
+    }
 }
